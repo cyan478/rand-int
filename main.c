9ef4d3fc61fc5fa82167ecaeeb9298c71c5257fc
whereami <jiaweichen29@gmail.com>
2016-10-27 01:23:19 +0000

    lol xdd

--- main.c
+++ main.c
@@ -10,11 +10,35 @@ int randomNumber () {
     return n;
 }
 
+void read_array(char* filename, int arr[10]) {
+    int fd = open(filename, O_RDONLY);
+    read(fd,arr,4*10);
+    close(fd);
+    return;
+}
+
+void write_array(char*filename, int arr[10]) {
+    int fd = open(filename, O_WRONLY|O_CREAT); 
+    write(fd,arr,4*10);
+    close(fd);
+    return;
+}
+
 int main() {
     int random[10];
     int n;
     for (n=0;n<10;n++) {
         random[n] = randomNumber();
         printf("Random %d: %d\n",n,random[n]);
+    }
+    
+    write_array("test",random);
+    
+    int testarray[10]; 
+    
+    read_array("test",testarray);
+    
+    for (n=0;n<10;n++) {
+        printf("New Array %d: %d\n",n,testarray[n]);
     }
 }
